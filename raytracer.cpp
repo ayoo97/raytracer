e1b01dc22881047b038a7145dc5d26c6f72570da
Alex <ayoo97@gmail.com>
2020-12-07 20:32:41 -0800

    Added a progress indicator

--- raytracer.cpp
+++ raytracer.cpp
@@ -13,6 +13,10 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height ; j >= 0 ; --j) {
+        
+        // this is a Progress Indicator
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        
         for (int i = 0 ; i < image_width ; ++i) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
