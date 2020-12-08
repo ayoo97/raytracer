3957156741b3ba6e7b0c8966dc8feb9372de53e1
Alex <ayoo97@gmail.com>
2020-12-08 13:43:37 -0800

    Added color.h and vec3.h

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 
+#include "color.h"
+#include "vec3.h"
+
 int main() {
     
     // image
@@ -19,7 +22,7 @@ int main() {
         // ---------------------------------------------------------------   
 
         for (int i = 0 ; i < image_width ; ++i) {
-            auto r = double(i) / (image_width - 1);
+            /*auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
             auto b = 0.25;
 
@@ -27,7 +30,11 @@ int main() {
             int ig = static_cast<int>(255.999 * g);
             int ib = static_cast<int>(255.999 * b);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';*/
+            
+            // instead of the loop above, use write_color() from color.h
+            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            write_color(std::cout, pixel_color);
         }
     }
 
